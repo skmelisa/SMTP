226c6ac24534c2e76c557ff762d1680998640ac3
Melisa Sultan Kırpık <melisasltnkrpk123@gmail.com>
2024-11-09 19:52:28 +0300

    Initial commit.

---
+++ Email/Email/Controllers/GmailController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Email.Models;
+using System.Web.Mvc;
+
+namespace Email.Controllers
+{
+    public class GmailController : Controller
+    {
+        // GET: Gmail
+        public ActionResult Send()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Send(Gmail gmail)
+        {
+            gmail.SendMail();
+            return View();
+        }
+    }
+}
